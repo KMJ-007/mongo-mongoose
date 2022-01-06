2e7d2f50c0c46eaa172bb72bc178b38ebcdcbcc5
Karan Janthe <karanjanthe@gmail.com>
2022-01-06 13:16:52 +0530

    created many model

--- myApp.js
+++ myApp.js
@@ -34,10 +34,13 @@ const createAndSavePerson = (done) => {
 
   });
 };
-
+const arrayOfPeople=[{name:'Krishna Agarwal',age:40,favoriteFoods:['grilledsandwich']},{name:'Het bhavsar',age:21,favoriteFoods:['pasta']}]
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople,(err,data)=>{
+    if(err) return  console.error(err);
+    done(null , data);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
