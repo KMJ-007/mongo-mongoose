d1f9eeeb6025a9d2d16c93d438cab753de2891f0
Karan Janthe <karanjanthe@gmail.com>
2022-01-06 14:36:53 +0530

    function removedbyid

--- myApp.js
+++ myApp.js
@@ -97,7 +97,10 @@ const findAndUpdate = (personName, done) => {
 
   
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId,(err,removedPerson)=>{
+    if(err) return console.log(err);
+    done(null,removedPerson);
+  })
 };
 
 const removeManyPeople = (done) => {
