a14a4cf47e75011dd543c1c30eba2952529430d8
Karan Janthe <karanjanthe@gmail.com>
2022-01-06 14:04:34 +0530

    updated findandeditandsave

--- myApp.js
+++ myApp.js
@@ -72,8 +72,17 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
+  Person.findById({_id:personId},(err,data)=>{
+    if(err)return console.log(err);
+    
+    Person.favoriteFoods.push(foodToAdd);
 
-  done(null /*, data*/);
+    person.save((err,updatedPerson)=>{
+      if(err)return console.log(err);
+    done(null,updatedPerson);
+    })
+  });
+
 };
 
 const findAndUpdate = (personName, done) => {
